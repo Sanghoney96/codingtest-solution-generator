b5dea250cabddf19730fae5968f6feef82d5f5b1
Sanghoney96 <shultra2@gmail.com>
2025-12-24 15:02:50 +0900

    final modifications for SFT pipeline

--- make_prompts.py
+++ make_prompts.py
@@ -1,4 +1,5 @@
 from prompts import COT_SYSTEM_PROMPT
+from datasets import Dataset
 
 def generate_prompts(dataset, tokenizer, is_eval=False):
     output_texts = []
@@ -8,19 +9,19 @@ def generate_prompts(dataset, tokenizer, is_eval=False):
             system_msg + COT_SYSTEM_PROMPT 
             + "### Question:" + user_msg
         )
-        
-        chunks = response.split("```")
-        reasoning = chunks[0]
-        code = chunks[1] if len(chunks) > 1 else ""
-        explanation = chunks[2] if len(chunks) > 2 else ""
-        
-        assistant_msg = (
-            reasoning + "\n\n"
-            + explanation + "\n\n"
-            + "```" + code + "\n\n```"
-        )
 
         if is_eval == False:
+            chunks = response.split("```")
+            reasoning = chunks[0]
+            code = chunks[1] if len(chunks) > 1 else ""
+            explanation = chunks[2] if len(chunks) > 2 else ""
+            
+            assistant_msg = (
+                reasoning + "\n\n"
+                + explanation + "\n\n"
+                + "```" + code + "\n\n```"
+            )
+            
             messages = [
                     {"role": "user", "content": cot_user_msg},
                     {"role": "assistant", "content": assistant_msg}
@@ -30,35 +31,15 @@ def generate_prompts(dataset, tokenizer, is_eval=False):
                                                 add_generation_prompt=False)
         else:
             messages = [
-                    {"role": "user", "content": cot_user_msg}
-                ]
+                {"role": "system", "content": system_msg + COT_SYSTEM_PROMPT},
+                {"role": "user", "content": "### Question:" + user_msg}
+            ]
             prompt = tokenizer.apply_chat_template(messages, 
                                                 tokenize=False, 
                                                 add_generation_prompt=True)
             
         output_texts.append(prompt)
         
+    output_texts = Dataset.from_dict({"text": output_texts})
+    
     return output_texts
-
-
-def preprocess_inputs(df):
-    system_msg, user_msg = df['query'].split("### Question:", 1)
-    cot_prompt = system_msg + COT_SYSTEM_PROMPT + "### Question: " + user_msg
-    
-    chunks = df["response"].split("```")
-    reasoning = chunks[0]
-    code = chunks[1] if len(chunks) > 1 else ""
-    explanation = chunks[2] if len(chunks) > 2 else ""
-    
-    completion = (
-        reasoning
-        + explanation + "\n\n"
-        + "```" + code + "\n```"
-    )
-    
-    return {
-        "prompt": [{"role": "user", "content": cot_prompt}],
-        "completion": [
-            {"role": "assistant", "content": completion}
-        ],
-    }
